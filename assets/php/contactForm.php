4b80472b9d08bfe7ab9f947af7632aa018d1d167
brian <bmarsha72@gmail.com>
2016-05-12 20:39:10 -0500

    more email experiments

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -8,10 +8,10 @@ $mail = new PHPMailer;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp1.gmail.com;smtp2.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'user@example.com';                 // SMTP username
-$mail->Password = 'secret';                           // SMTP password
+$mail->Username = 'bmarsha72@gmail.com';                 // SMTP username
+$mail->Password = 'superfly123';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
